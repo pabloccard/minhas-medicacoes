aae4886e047597771b0a1bf6601928c36d8c2b8e
pabloccard <pabloccard@gmail.com>
2023-05-09 07:45:10 -0300

    feat(Header): Add active link

---
+++ src/app/medicamentos/page.tsx
@@ -0,0 +1,3 @@
+export default function Medicamentos() {
+  return <h1>Medicamentos</h1>
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import styles from './styles.module.css'
 import {
@@ -5,26 +7,42 @@ import {
   AiOutlineHome,
   AiOutlineMedicineBox,
 } from 'react-icons/ai'
+import { usePathname } from 'next/navigation'
 
 export const Header = () => {
+  const pathname = usePathname()
+  console.log(pathname)
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <strong className={styles.logo}>Minhas Medicações</strong>
         <nav>
-          <Link href="/" className={`${styles.link} ${styles.active}`}>
+          <Link
+            href="/"
+            className={`${styles.link} ${pathname === '/' && styles.active}`}
+          >
             <span>
               <AiOutlineHome />
             </span>
             <strong>Home</strong>
           </Link>
-          <Link href="/calendar" className={styles.link}>
+          <Link
+            href="/calendario"
+            className={`${styles.link} ${
+              pathname === '/calendario' && styles.active
+            }`}
+          >
             <span>
               <AiOutlineCalendar />
             </span>
             <strong>Calendário</strong>
           </Link>
-          <Link href="/calendar" className={styles.link}>
+          <Link
+            href="/medicamentos"
+            className={`${styles.link} ${
+              pathname === '/medicamentos' && styles.active
+            }`}
+          >
             <span>
               <AiOutlineMedicineBox />
             </span>
