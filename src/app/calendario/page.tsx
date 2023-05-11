a50712ffbc764aa385eb97ac878bc10f8d7a1692
pabloccard <pabloccard@gmail.com>
2023-05-11 13:58:46 -0300

    feat(Calendario): Add all components

--- src/app/calendario/page.tsx
+++ src/app/calendario/page.tsx
@@ -1,3 +1,62 @@
+import styles from './styles.module.css'
+
 export default function Calendario() {
-  return <h1>Calendar</h1>
+  return (
+    <div className={styles.container}>
+      <div className={styles.selectedDate}>
+        <strong>7</strong>
+        <div>
+          <span>Terça</span>
+          <span>Abril | 2021</span>
+        </div>
+      </div>
+
+      <div className={styles.scheduledDayList}>
+        <button className={`${styles.scheduledDay} ${styles.active}`}>
+          <strong>Ter</strong>
+          <strong>12</strong>
+        </button>
+        <button className={styles.scheduledDay}>
+          <strong>qua</strong>
+          <strong>13</strong>
+        </button>
+        <button className={styles.scheduledDay}>
+          <strong>Qui</strong>
+          <strong>14</strong>
+        </button>
+      </div>
+
+      <div className={styles.scheduledTimeContainer}>
+        <table className={styles.scheduledDayTable}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>medicamento</th>
+              <th>horário</th>
+              <th>status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>
+                <input type="checkbox" name="" id="" />
+              </td>
+              <td>Amoxilina</td>
+              <td>22:45</td>
+              <td>aguardando</td>
+            </tr>
+            <tr>
+              <td>
+                <input type="checkbox" name="" id="" />
+              </td>
+              <td>Amoxilina</td>
+              <td>22:45</td>
+              <td>aguardando</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
