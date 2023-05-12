c87783ca312faac59791f1bd379f7e026229c0ee
pabloccard <pabloccard@gmail.com>
2023-05-12 14:03:35 -0300

    feat(calendario): Add slider

--- src/app/calendario/page.tsx
+++ src/app/calendario/page.tsx
@@ -1,6 +1,48 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import styles from './styles.module.css'
+import { useKeenSlider } from 'keen-slider/react'
 
 export default function Calendario() {
+  const [activeSlide, setActiveSlide] = useState(false)
+
+  const [sliderRef, instanceRef] = useKeenSlider({
+    initial: 0,
+    slides: {
+      perView: 'auto',
+      spacing: activeSlide ? 8 : 0,
+      // origin: 'center',
+    },
+
+    range: {
+      // align: true,
+    },
+
+    breakpoints: {
+      '(min-width: 1024px)': {
+        slides: {
+          perView: 2.2,
+          spacing: 16,
+        },
+      },
+    },
+  })
+
+  useEffect(() => {
+    if (instanceRef.current) {
+      const sliderWidth = instanceRef.current.size
+      const itemsWidthSum = instanceRef.current.slides.length * 76
+      const spacingWidthSum = (instanceRef.current.slides.length - 1) * 8
+
+      if (sliderWidth < itemsWidthSum + spacingWidthSum) {
+        setActiveSlide(true)
+      }
+
+      console.log(sliderWidth < itemsWidthSum + spacingWidthSum)
+    }
+  }, [instanceRef])
+
   return (
     <div className={styles.container}>
       <div className={styles.selectedDate}>
@@ -11,19 +53,40 @@ export default function Calendario() {
         </div>
       </div>
 
-      <div className={styles.scheduledDayList}>
-        <button className={`${styles.scheduledDay} ${styles.active}`}>
-          <strong>Ter</strong>
-          <strong>12</strong>
-        </button>
-        <button className={styles.scheduledDay}>
-          <strong>qua</strong>
-          <strong>13</strong>
-        </button>
-        <button className={styles.scheduledDay}>
-          <strong>Qui</strong>
-          <strong>14</strong>
-        </button>
+      <div
+        className={`${styles.scheduledDayList} ${
+          activeSlide && styles.activeSlide
+        }`}
+      >
+        <div
+          ref={sliderRef}
+          className={`${styles.slider} ${
+            activeSlide && styles.activeSlide
+          } keen-slider `}
+        >
+          <button
+            className={`${styles.scheduledDay} ${styles.active} keen-slider__slide`}
+          >
+            <strong>Ter</strong>
+            <strong>12</strong>
+          </button>
+          <button className={`${styles.scheduledDay} keen-slider__slide`}>
+            <strong>qua</strong>
+            <strong>13</strong>
+          </button>
+          <button className={`${styles.scheduledDay} keen-slider__slide`}>
+            <strong>qua</strong>
+            <strong>13</strong>
+          </button>
+          <button className={`${styles.scheduledDay} keen-slider__slide`}>
+            <strong>qua</strong>
+            <strong>13</strong>
+          </button>
+          <button className={`${styles.scheduledDay} keen-slider__slide`}>
+            <strong>qua</strong>
+            <strong>13</strong>
+          </button>
+        </div>
       </div>
 
       <div className={styles.scheduledTimeContainer}>
@@ -38,6 +101,30 @@ export default function Calendario() {
           </thead>
 
           <tbody>
+            <tr>
+              <td>
+                <input type="checkbox" name="" id="" />
+              </td>
+              <td>Amoxilina</td>
+              <td>22:45</td>
+              <td>aguardando</td>
+            </tr>
+            <tr>
+              <td>
+                <input type="checkbox" name="" id="" />
+              </td>
+              <td>Amoxilina</td>
+              <td>22:45</td>
+              <td>aguardando</td>
+            </tr>
+            <tr>
+              <td>
+                <input type="checkbox" name="" id="" />
+              </td>
+              <td>Amoxilina</td>
+              <td>22:45</td>
+              <td>aguardando</td>
+            </tr>
             <tr>
               <td>
                 <input type="checkbox" name="" id="" />
