0600e11331d69651fc669798cfec93b0e0629dfb
pabloccard <pabloccard@gmail.com>
2023-05-17 19:00:13 -0300

    feat: add dateFormatter

---
+++ src/utils/formatter.ts
@@ -0,0 +1,37 @@
+import { format, formatISO9075 } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+export default function dateFormatter() {
+  const formatToWeekDay = (date: Date): string => {
+    return format(date, 'eee', { locale: ptBR })
+  }
+
+  const formatToShortWeekDay = (date: Date): string => {
+    return format(date, 'EEEEEE', { locale: ptBR })
+  }
+
+  const formatToDay = (date: Date): string => {
+    return format(date, 'd', { locale: ptBR })
+  }
+
+  const formatToMonth = (date: Date): string => {
+    return format(date, 'MMMM', { locale: ptBR })
+  }
+
+  const formatToYear = (date: Date): string => {
+    return format(date, 'yyyy', { locale: ptBR })
+  }
+
+  const formatToTime = (date: Date): string => {
+    return formatISO9075(date, { representation: 'time' })
+  }
+
+  return {
+    formatToWeekDay,
+    formatToShortWeekDay,
+    formatToDay,
+    formatToMonth,
+    formatToYear,
+    formatToTime,
+  }
+}
